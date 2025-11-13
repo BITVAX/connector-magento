4ab63188e3d28dc8ef931d6b14ea2ac0d2e0acb2
Ignacio J. Ortega <nacho@apache.org>
2025-11-13 14:50:30 +0100

    reorder imports

--- connector_magento/models/__init__.py
+++ connector_magento/models/__init__.py
@@ -5,7 +5,6 @@ from . import magento_taxclass
 from . import magento_attribute_set
 from . import product_attribute
 from . import product_attribute_value
-from . import product_template
 from . import product_template_attribute_line
 from . import account_invoice
 from . import account_payment_mode
@@ -17,6 +16,7 @@ from . import magento_website
 from . import partner
 from . import partner_category
 from . import product
+from . import product_template
 from . import product_category
 from . import queue_job
 from . import sale_order
