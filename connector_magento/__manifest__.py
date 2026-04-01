f38df7c09c8c1dc17e40e3880dcb8e0c166af1e9
Ignacio J. Ortega <nacho.ortega@gmail.com>
2026-04-01 17:36:29 +0200

    [MIG] connector_magento: version bump to 18.0

--- connector_magento/__manifest__.py
+++ connector_magento/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {'name': 'Magento Connector',
- 'version': '16.0.0.0.0',
+ 'version': '18.0.1.0.0',
  'category': 'Connector',
  'depends': ['account',
              'base_technical_user',
